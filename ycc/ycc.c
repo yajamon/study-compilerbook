05558181a3258f440a96172ed0edd725b86a29b3
yajamon <yajamon.tatsuki@gmail.com>
2018-11-03 11:22:23 +0900

    最初のトークンを解釈する処理をTokenを用いたものにした

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -54,20 +54,30 @@ void tokenize(char *p) {
     tokens[i].input = p;
 }
 
+// エラー報告関数
+void token_error(int i) {
+    fprintf(stderr, "予期しないトークンを検出しました: '%s'\n", tokens[i].input);
+    exit(1);
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の数が正しくありません\n");
         return 1;
     }
 
-    char *p = argv[1];
+    tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main, _main\n");
     printf("\n");
     printf("main:\n");
 
-    printf("	mov rax, %ld\n", strtol(p, &p, 10));
+    // 式の最初は数である必要がある
+    if (tokens[0].type_code != TK_NUM) {
+        token_error(0);
+    }
+    printf("	mov rax, %d\n", tokens[0].value);
 
     while (*p) {
         if (*p == '+') {
