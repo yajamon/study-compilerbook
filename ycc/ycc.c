270204cbccbac3b10b570b8411e06959c964a165
yajamon <yajamon.tatsuki@gmail.com>
2018-11-03 09:34:08 +0900

    strtol関数を使って引数最初の数字を取得する
    strtol関数は第一引数で与えられた文字列を、第三引数に指定された基数でlong型に変換して返す。
    数値に変換不可能な文字があった場合は、その文字列のポインタを第2引数に格納する。
    
    この事例では、long型に変換した値を得ながら、ポインタの指すアドレスを
    数値に変換出来ない文字まで進行させる。

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -7,11 +7,15 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main, _main\n");
     printf("\n");
     printf("main:\n");
-    printf("	mov rax, %d\n", atoi(argv[1]));
+
+    printf("	mov rax, %ld\n", strtol(p, &p, 10));
+
     printf("	ret\n");
     printf("\n");
     printf("_main:\n");
