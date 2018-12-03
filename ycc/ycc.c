f38d27fa41546381ed476ff6706445118bc51396
yajamon <yajamon.tatsuki@gmail.com>
2018-12-03 22:08:57 +0900

    複数行のコード、および変数を実現するコードを出力できるようにした
    テストケースの末尾に;を付けるように改変した。
    言語構文が変化してしまったんだもの。しょうがないね！！

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -288,19 +288,33 @@ int main(int argc, char **argv) {
     }
 
     tokenize(argv[1]);
-    Node *rootNode = expr();
+    // パース。結果は code に保存される
+    program();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main, _main\n");
     printf("\n");
     printf("main:\n");
 
-    // 抽象構文木を下りながらコード生成
-    gen(rootNode);
+    // プロローグ
+    // 変数26個分の領域を確保する
+    printf("	push rbp\n");
+    printf("	mov rbp, rsp\n");
+    printf("	sub rsp, 208\n"); // 8 * 26 = 208
 
-    // スタックトップに式全体の値があるはずなので
-    // それをraxにロードする
-    printf("	pop rax\n");
+    // 先頭の式から順にコード生成
+    for (int i=0; code[i]; i++){
+        gen(code[i]);
+
+        // 式の評価結果としてスタックに1つの値が残る
+        // これが積み重なって溢れないよう、ポップしておく
+        printf("	pop rax\n");
+    }
+
+    // エピローグ
+    // 最後の式の結果がraxに残っているので、それが返り値となる
+    printf("	mov rsp, rbp\n");
+    printf("	pop rbp\n");
 
     printf("	ret\n");
     printf("\n");
