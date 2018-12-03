10e02ed460a3838099bb530fac00cd67a2d6e83a
yajamon <yajamon.tatsuki@gmail.com>
2018-12-03 23:01:32 +0900

    トークン化処理を分割

---
+++ ycc/tokenize.c
@@ -0,0 +1,60 @@
+#include "ycc.h"
+
+// エラー報告関数
+void token_error(int i) {
+    error("予期しないトークンを検出しました: '%s'\n", tokens[i].input);
+}
+
+// pが指している文字列をトークンに分割してtokensに保存する
+void tokenize(char *p) {
+    int i = 0;
+    while (*p) {
+        // トークン可の検証用処理
+        // fprintf(stderr, "target: %s\n", p);
+
+        // 空白文字をスキップ
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        switch (*p) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '(':
+            case ')':
+            case '=':
+            case ';':
+                tokens[i].type_code = *p;
+                tokens[i].input = p;
+                i++;
+                p++;
+                continue;
+            default:
+                break;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].type_code = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].value = strtol(p, &p, 10);
+            i++;
+            continue;
+        }
+
+        if ('a' <= *p && *p <= 'z') {
+            tokens[i].type_code = TK_IDENT;
+            tokens[i].input = p;
+            i++;
+            p++;
+            continue;
+        }
+
+        error("トークナイズできません: %s\n", p);
+    }
+
+    tokens[i].type_code = TK_EOF;
+    tokens[i].input = p;
+}
--- ycc/ycc.c
+++ ycc/ycc.c
@@ -13,67 +13,6 @@ void error(char* format, char* param) {
 // トークナイズした結果のトークン列を保存する配列
 // 100個以上のトークンはこないものとする
 Token tokens[100];
-
-// pが指している文字列をトークンに分割してtokensに保存する
-void tokenize(char *p) {
-    int i = 0;
-    while (*p) {
-        // トークン可の検証用処理
-        // fprintf(stderr, "target: %s\n", p);
-
-        // 空白文字をスキップ
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        switch (*p) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-            case '(':
-            case ')':
-            case '=':
-            case ';':
-                tokens[i].type_code = *p;
-                tokens[i].input = p;
-                i++;
-                p++;
-                continue;
-            default:
-                break;
-        }
-
-        if (isdigit(*p)) {
-            tokens[i].type_code = TK_NUM;
-            tokens[i].input = p;
-            tokens[i].value = strtol(p, &p, 10);
-            i++;
-            continue;
-        }
-
-        if ('a' <= *p && *p <= 'z') {
-            tokens[i].type_code = TK_IDENT;
-            tokens[i].input = p;
-            i++;
-            p++;
-            continue;
-        }
-
-        error("トークナイズできません: %s\n", p);
-    }
-
-    tokens[i].type_code = TK_EOF;
-    tokens[i].input = p;
-}
-
-// エラー報告関数
-void token_error(int i) {
-    error("予期しないトークンを検出しました: '%s'\n", tokens[i].input);
-}
-
-
 Node *code[100] = {NULL};
 
 
