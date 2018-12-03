b4d86a94677858913fca07224c8d0f824a111378
yajamon <yajamon.tatsuki@gmail.com>
2018-12-03 21:51:39 +0900

    コード生成部を改修: 変数の代入、読み出しができるようになった
    左辺値としてコードを生成する関数を作成
    右辺値としてのND_IDENTの処理を作成
    代入処理を作成

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -218,11 +218,45 @@ Node* term() {
     error("数字でも開きカッコでもないトークンです: %s\n", tokens[pos].input);
 }
 
+void gen_lvalue(Node *node) {
+    if (node->type_code == ND_IDENT) {
+        printf("	mov rax, rbp\n");
+        printf("	sub rax, %d\n",
+                ('z' - node->name + 1) * 8);
+        printf("	push rax\n");
+        return;
+    }
+    error("代入の左辺値が変数ではありません", "");
+}
+
 void gen(Node *node) {
     if (node->type_code == ND_NUM) {
         printf("	push %d\n", node->value);
         return;
     }
+
+    if (node->type_code == ND_IDENT) {
+        gen_lvalue(node);
+        printf("	pop rax\n");
+        printf("	mov rax, [rax]\n");
+        printf("	push rax\n");
+        return;
+    }
+
+    if (node->type_code == '=') {
+        // 変数のアドレスも、右辺値もとりあえずスタックに積む
+        gen_lvalue(node->lhs);
+        gen(node->rhs);
+
+        // rax(左辺値)のアドレスにrdi(右辺値)を書き込む
+        printf("	pop rdi\n");
+        printf("	pop rax\n");
+        printf("	mov [rax], rdi\n");
+        // 右辺値としての値を返してあげないと、a=b=1が成立しなくなる
+        printf("	push rdi\n");
+        return;
+    }
+
     gen(node->lhs);
     gen(node->rhs);
 
