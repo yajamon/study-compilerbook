f61033ce88e0f9fd04e5cacdcd85a6b787891f7c
yajamon <yajamon.tatsuki@gmail.com>
2020-05-29 19:07:27 +0900

    Tokenを定義する。

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    TK_RESERVED,    // 記号
+    TK_NUM,         // 数値
+    TK_EOF,         // 終端
+} TokenKind;
+
+struct Token {
+    TokenKind kind;
+    int val;            // kind が TK_NUM の場合、その数値
+    char* str;           // Tokenの文字列
+};
+
 int main(int argc, char const* argv[])
 {
     if (argc != 2) {
