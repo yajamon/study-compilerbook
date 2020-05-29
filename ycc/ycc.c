80b973a43d5ee1d01d4c7d0feab4b74495b9cd63
yajamon <yajamon.tatsuki@gmail.com>
2020-05-29 18:04:56 +0900

    最初の数字を読み込む
    整数以外の文字が登場するため、strtol関数を使う。
    この関数は第一引数のポインタから、第三引数の基数で文字を解釈していく。
    数値ではない文字が見つかったら、そのアドレス値を第二引数に与える。
    
    `strtol(p, &p, 10)`とすると、ポインタ`p`を数値の次の文字まで移動できる。

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -8,11 +8,13 @@ int main(int argc, char const* argv[])
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
     printf("    ret\n");
     return 0;
 }
