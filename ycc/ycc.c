7498e2490f7f66d242c8e44d7aa87243d3d0f506
yajamon <yajamon.tatsuki@gmail.com>
2020-07-27 19:33:38 +0900

    抽象構文木を用いたコード生成処理を作成する

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -160,6 +160,30 @@ Node* expr() {
     }
 }
 
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
