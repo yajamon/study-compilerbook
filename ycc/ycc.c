8a32ae8a40b7a2116a5be5281dbe82348386e7c5
yajamon <yajamon.tatsuki@gmail.com>
2020-07-29 01:21:45 +0900

    Green: 比較演算子 == に対応したアセンブリを出力する

--- ycc/ycc.c
+++ ycc/ycc.c
@@ -256,6 +256,11 @@ void gen(Node *node) {
     printf("    cqo\n");
     printf("    idiv rax, rdi\n");
     break;
+  case ND_EQ:
+    printf("    cmp rax, rdi\n");
+    printf("    sete al\n");
+    printf("    movzb rax, al\n");
+    break;
   }
 
   printf("    push rax\n");
